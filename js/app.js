d44766ef01ea7e55cc8a9da68fc224a64c7bd4f4
goodbedford <bedford@goodbedford.com>
2016-07-30 11:19:30 -0700

    styles, basic functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,260 @@
 
 
 var Board = new Board(3,3);
+var game = new Game();
 var board = Board.getBoard();
 var boardTarget = document.querySelector(".gameboard-target");
-boardTarget.appendChild(board);
-var btn = document.querySelector(".btn-clear");
+var boardBlocker = document.createElement("div")
+boardBlocker.classList.add("board__blocker");
 
+
+activate();
+
+function activate() {
+  game.moveHandler(Board.getSquares(), game.getTurn);
+  board.appendChild(boardBlocker);
+  boardTarget.appendChild(board);
+
+}
+
+
+
+var btn = document.querySelector(".btn-clear");
 btn.addEventListener("click", function(e) {
   Board.clearBoard();
 });
+
+function Game() {
+  var onePlayerGame = false;
+  var twoPlayerGame = false;
+  var players = {player1:"", player2:""};
+  var numOfPlayers;
+  var start;
+  var gameTurn = "x";
+  var startBtn = document.querySelector("#startGame")
+  var onePlayerGameBtn = document.querySelector("#onePlayerGame");
+  var twoPlayerGameBtn = document.querySelector("#twoPlayerGame");
+  var gamePieceXBtn = document.querySelector("#gamePieceX");
+  var gamePieceOBtn = document.querySelector("#gamePieceO");
+
+  this.getStart = getStart;
+  this.rounds = 5;
+  this.winner;
+  this.loser;
+  // this.numOfPlayers;  // 1, 2
+  this.gameTurn;
+  // this.getPlayers = getPlayers; delete
+  this.getNumOfPlayer = getNumOfPlayer;
+  this.moveHandler = moveHandler;
+  this.getTurn = getTurn;
+  this.setTurn = setTurn;
+  // this.checkForWinnerHandler = checkForWinnerHandler;
+  step1Listener();
+  step2NumOfPlayers1Listener();
+  step2NumOfPlayers2Listener();
+  step3GamePieceXListener();
+  step3GamePieceOListener();
+  // step1 start
+  function step1Listener() {
+      startBtn.addEventListener("click", function(event) {
+        var parent = this.parentElement;
+        parent.classList.remove("onboard-steps-intro-ani");
+        parent.classList.add("onboard-steps-outro");
+        setStart();
+        var step2 = document.querySelector("#step2");
+        setTimeout(function() {
+          parent.classList.add("gameboard-instructions__card--hide");
+          step2.classList.add("onboard-steps-intro-ani");
+          step2.classList.add("gameboard-instructions__card--active");
+        }, 1000);
+      });
+  }
+  // step2 Number of players onePlayerGameBtn
+  function step2NumOfPlayers1Listener() {
+      onePlayerGameBtn.addEventListener("click", function(event) {
+        var parent = this.parentElement;
+        parent.classList.remove("onboard-steps-intro-ani");
+        parent.classList.add("onboard-steps-outro");
+        setPlayers(1);
+        var step3 = document.querySelector("#step3");
+        setTimeout(function() {
+          parent.classList.add("gameboard-instructions__card--hide");
+          step3.classList.add("onboard-steps-intro-ani");
+          step3.classList.add("gameboard-instructions__card--active");
+        }, 1000);
+      });
+  }
+  // step2 Number of players twoPlayerGameBtn
+  function step2NumOfPlayers2Listener() {
+      twoPlayerGameBtn.addEventListener("click", function(event) {
+        var parent = this.parentElement;
+        parent.classList.remove("onboard-steps-intro-ani");
+        parent.classList.add("onboard-steps-outro");
+        setPlayers(2);
+        var step3 = document.querySelector("#step3");
+        setTimeout(function() {
+          parent.classList.add("gameboard-instructions__card--hide");
+          step3.classList.add("onboard-steps-intro-ani");
+          step3.classList.add("gameboard-instructions__card--active");
+        }, 1000);
+      });
+  }
+  // step3 choose game piece
+  function step3GamePieceXListener() {
+      gamePieceXBtn.addEventListener("click", function(event) {
+        var parent = this.parentElement;
+        parent.classList.remove("onboard-steps-intro-ani");
+        parent.classList.add("onboard-steps-outro");
+        setGamePieceX();
+        setTurn("x");
+        // startGame(); delete
+        var step4 = document.querySelector("#step4");
+        setTimeout(function() {
+          boardBlocker.parentElement.removeChild(boardBlocker);
+
+          parent.classList.add("gameboard-instructions__card--hide");
+          step4.classList.add("onboard-steps-intro-ani");
+          step4.classList.add("gameboard-instructions__card--active");
+        }, 1000);
+      });
+  }
+  // step3 choose game piece
+  function step3GamePieceOListener() {
+      gamePieceOBtn.addEventListener("click", function(event) {
+        var parent = this.parentElement;
+        parent.classList.remove("onboard-steps-intro-ani");
+        parent.classList.add("onboard-steps-outro");
+        setGamePieceO();
+        setTurn("o");
+        // startGame(); delete
+        var step4 = document.querySelector("#step4");
+        setTimeout(function() {
+          // var blocker = document.querySelector(".board__blocker");
+          // console.log("blocelr", blocker);
+          // debugger;
+          boardBlocker.parentElement.removeChild(boardBlocker);
+
+          parent.classList.add("gameboard-instructions__card--hide");
+          step4.classList.add("onboard-steps-intro-ani");
+          step4.classList.add("gameboard-instructions__card--active");
+          // blocker.parentElement.removeChild(blocker);
+        }, 1000);
+      });
+  }
+
+  function setStart() {
+    start = true;
+  }
+  function getStart() {
+    return start;
+  }
+  function getNumOfPlayer() {
+    return players;
+  }
+  function setPlayers(numOfPlayers) {
+    numOfPlayers = numOfPlayers;
+  }
+  function setGamePieceX() {
+    players.player1 = "x";
+    players.player2 = "o";
+  }
+  function setGamePieceO(o) {
+    players.player1 = "o";
+    players.player2 = "x";
+  }
+  function setTurn(turn) {
+    console.log("setturn", turn);
+    var target = document.querySelector(".gameboard-instructions__game-state-container__state");
+    gameTurn = turn;
+    if(gameTurn == "x") {
+      document.querySelector("#player1").classList.add("gameboard-instructions__img-container--active");
+      document.querySelector("#player2").classList.remove("gameboard-instructions__img-container--active");
+      target.innerHTML = "<";
+    } else {
+      document.querySelector("#player2").classList.add("gameboard-instructions__img-container--active");
+      document.querySelector("#player1").classList.remove("gameboard-instructions__img-container--active");
+      target.innerHTML = ">";
+    }
+  }
+  function getTurn() {
+    return gameTurn;
+  }
+  function getPlayers() {
+      return players;
+  }
+  function startGame() {
+
+  }
+  function moveHandler(squares,currentTurn) {
+    squares.forEach(function(square, indx,self) {
+      square.addEventListener("click", function(event) {
+
+        if(this.innerHTML === "") {
+          if(currentTurn() == "x") {
+            this.innerHTML = "x";
+            checkForWinner(squares,currentTurn,players);
+
+            // setTurn("o"); check for winner first
+            // debugger;
+          } else {
+            this.innerHTML = "o";
+            checkForWinner(squares,currentTurn,players);
+
+            // setTurn("x"); // checkfor winner first
+          }
+        } else {
+          alert("Try another square!");
+        }
+      });
+    });
+  }
+  // function checkForWinnerHandler(squares, currentTurn, players) {
+  //   squares = squares();
+  //   squares.forEach(function(square,indx,self) {
+  //     square.addEventListener("click", function(event) {
+  //       checkForWinner(squares,currentTurn,players);
+  //     });
+  //   });
+  // }
+  function lockGame() {
+    var board = document.querySelector(".board");
+    var boardBlocker = document.createElement("div");
+    boardBlocker.classList.add("board__blocker");
+    board.appendChild(boardBlocker);
+    board.addEventListener("click", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      console.log("do nothing.s");
+    });
+  }
+  function checkForWinner(squares, currentTurn) {
+    var winner;
+    var turn = "";
+    var squares = squares;
+    console.log("checking", currentTurn());
+
+    if (currentTurn() == "x") {
+      turn = "x";
+    } else {
+      turn = "o";
+    }
+      console.log("checking", currentTurn(), turn);
+    if (squares[0].innerHTML == turn &&
+        squares[1].innerHTML == turn  &&
+        squares[2].innerHTML == turn) {
+          squares[0].classList.add("gameboard-winner-ani");
+          squares[1].classList.add("gameboard-winner-ani");
+          squares[2].classList.add("gameboard-winner-ani");
+
+        lockGame();
+        return winner = turn;
+
+    } else {
+      if(turn == "x") {
+        setTurn("o");
+      } else {
+          setTurn("x");
+      }
+    }
+  }
+}
--- js/board.js
+++ js/board.js
@@ -1,34 +1,107 @@
 
 
-function Board(rows, squares) {
-  var makeBoard = function() {
+function Board(numOfRows, squaresPerRow) {
+  var makeBoard = makeBoard();
+  var rows;
+  var rowsLen;
+  var squares;
+  this.board = makeBoard;
+  this.getBoard = getBoard;
+  this.clearBoard = clearBoard;
+  this.addClassToSquares = addClassToSquares;
+  this.getRows = getRows;
+  this.getSquares = getSquares;
+  rows = this.getRows();
+  rowsLen = rows.length;
+  squares = this.getSquares();
+
+  activateListeners();
+
+  function activateListeners() {
+    handleHover();
+  }
+  function getRows() {
+    var rows = this.board.querySelectorAll(".board__row");
+    rows = Array.prototype.slice.call(rows);
+    return rows;
+  }
+  function getSquares() {
+    var squares = [];
+    var temp;
+    rows.forEach(function(row){
+      temp = row.querySelectorAll(".board__row__square");
+      temp = Array.prototype.slice.call(temp);
+      squares = squares.concat(temp);
+    });
+    return squares;
+  }
+
+  function handleHover() {
+    squares.forEach(function(square) {
+      square.addEventListener("mouseenter", function(event) {
+        square.classList.add("gameboard-hover-ani");
+      });
+      square.addEventListener("mouseleave", function(event) {
+        square.classList.remove("gameboard-hover-ani");
+      });
+    });
+  }
+  // function handleMoves() {
+  //   squares.forEach(function(square) {
+  //     square.addEventListener("click", function(event) {
+  //       if(this.innerHTML) {
+  //         this.innerHTML = move;
+  //       }
+  //     })(move);
+  //     square.addEventListener("mouseleave", function(event) {
+  //       square.classList.remove("gameboard-hover-ani");
+  //     });
+  //   });
+  // }
+  function addClassToSquares(className) {
+    var rows = this.board.querySelectorAll(".board__row");
+    var rowsLen = rows.length;
+    var squares;
+
+    for(i = 0; i < rowsLen; i++) {
+      squares =  rows[i].querySelectorAll(".board__row__square");
+      squares.forEach(function(square) {
+        square.classList.add(className);
+      });
+    }
+  }
+  function makeBoard() {
     var board = document.createElement("div");
     var i = 0;
     board.classList.add("board");
 
-    for(i = 0; i < rows; i++) {
-      board.appendChild(makeRow(squares));
+    for(i = 0; i < numOfRows; i++) {
+      board.appendChild(makeRow(squaresPerRow));
     }
     return board;
   }
-  this.board = makeBoard();
-  this.getBoard = function() {
+  function getBoard() {
     return this.board;
   }
-  this.clearBoard = function() {
-    var rowsToClear;
-    var rowsLen = 0;
+  function clearBoard() {
+    var rows = this.board.querySelectorAll(".board__row");
+    var rowsLen = rows.length;;
     var squaresToClear;
-    rowsToClear = this.board.querySelectorAll(".board__row");
-    rowsLen = rowsToClear.length;
+    var blocker = document.querySelector(".board__blocker");
+
+    board.className = "board";
+    if(blocker) {
+      blocker.parentElement.removeChild(blocker);
+    }
+
     for(i = 0; i < rowsLen; i++) {
-      squaresToClear =  rowsToClear[i].querySelectorAll(".board__row__square");
+      squaresToClear =  rows[i].querySelectorAll(".board__row__square");
       squaresToClear.forEach(function(square) {
         square.innerHTML = "";
+        square.className = "board__row__square"
       });
     }
   }
-
 }
 
 function makeRow(squares) {
@@ -39,7 +112,6 @@ function makeRow(squares) {
   for (var i = 0; i < squares; i++) {
     square = document.createElement("div");
     square.classList.add("board__row__square");
-    square.innerHTML = "X";
     row.appendChild(square);
   }
   return row;
